3d2bcb157c224fa3241fe3ee63caed0601c01a8f
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-02-20 14:15:07 +0100

    data downloader

--- tools/download-data.sh
+++ tools/download-data.sh
@@ -11,18 +11,23 @@ FILES=(
     "CRNX/V3/MOJN00DNK_R_20201770000_01D_30S_MO.crx.gz"
     "NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz"
     "NAV/V3/MOJN00DNK_R_20201770000_01D_MN.rnx.gz"
+    "MET/V3/POTS00DEU_R_20232540000_01D_05M_MM.rnx.gz"
+    "IONEX/V1/jplg0010.17i.gz"
+    "CLK/V3/GRG0MGXFIN_20201770000_01D_30S_CLK.CLK.gz"
 )
     
 mkdir -p $LOCAL_DIR
+cd $LOCAL_DIR
 
 echo "Downloading test/example data..."
 
 for file in "${FILES[@]}"; do
     file_name=$(basename $file)
     wget -q \
-        --show-progress $GEORUST_URL/$file \
-        -o $LOCAL_DIR/$file_name
+        --show-progress $GEORUST_URL/$file
 done
 
 echo "Test data downloaded!"
-ls -lah $LOCAL_DIR/*
+
+cd ..
+ls -lah data/
