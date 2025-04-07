7ddb64a90a9b086a9bfc04a5c19a43e8121083e8
gwbres <guillaume.bressaix@gmail.com>
2025-04-07 22:08:38 +0200

    Update (#25)
    * Update documentation: Filegen options
    * File operations: small improvements
       * correct .csv extension
       * correct .gzip encoding
    * update documentation
    * Upgrade to rinex v0.17.1
    
    ---------
    
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/fops/filegen.rs
+++ src/fops/filegen.rs
@@ -10,22 +10,22 @@ use crate::fops::csv::{
     write_obs_rinex as write_obs_rinex_csv, write_raw_nav_rinex as write_raw_nav_rinex_csv,
 };
 
-/*
- * Dumps current context (usually preprocessed)
- * into either RINEX/SP3 format (maintaining consistent format) or CSV
- */
+/// Dumps current (possibly preprocessed) context
+/// into either RINEX / SP3 format (maintaining consistent format) or CSV
 pub fn filegen(ctx: &Context, matches: &ArgMatches, submatches: &ArgMatches) -> Result<(), Error> {
     #[cfg(feature = "csv")]
     if submatches.get_flag("csv") {
         write_csv(ctx, matches, submatches)?;
         return Ok(());
     }
+
     #[cfg(not(feature = "csv"))]
     if submatches.get_flag("csv") {
         panic!("Not available. Activate `csv` feature first.")
     }
 
     write(ctx, matches, submatches)?;
+
     Ok(())
 }
 
@@ -114,9 +114,15 @@ fn write(ctx: &Context, matches: &ArgMatches, submatches: &ArgMatches) -> Result
                 .to_string_lossy()
                 .to_string();
 
-            rinex.to_file(&output_path).unwrap_or_else(|_| {
-                panic!("failed to generate {} RINEX \"{}\"", product, output_path)
-            });
+            if submatches.get_flag("gzip") {
+                rinex.to_gzip_file(&output_path).unwrap_or_else(|_| {
+                    panic!("failed to generate {} RINEX \"{}\"", product, output_path)
+                });
+            } else {
+                rinex.to_file(&output_path).unwrap_or_else(|_| {
+                    panic!("failed to generate {} RINEX \"{}\"", product, output_path)
+                });
+            }
 
             info!("{} RINEX \"{}\" has been generated", product, output_path);
         }
